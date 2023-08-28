75948daf843ed4e42524e16634252befe733e492
Raiane <raiane.oliveira404@gmail.com>
2023-08-28 12:07:37 -0300

    fix: add ref to TextInput and adjust styles

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @raiane-ignite-ui/react
 
+## 3.0.1
+
+### Patch Changes
+
+- Add option to pass reference to TextInput and adjust some styles
+
 ## 3.0.0
 
 ### Major Changes
--- packages/react/src/components/TextInput/styles.ts
+++ packages/react/src/components/TextInput/styles.ts
@@ -8,7 +8,18 @@ export const TextInputContainer = styled('div', {
   border: '2px solid $gray900',
 
   display: 'flex',
-  alignItems: 'baseline',
+  alignItems: 'center',
+
+  variants: {
+    size: {
+      sm: {
+        padding: '$2 $3',   
+      },
+      md: {
+        padding: '$3 $4',
+      }
+    },
+  },
 
   '&:has(input:focus)': {
     borderColor: '$ignite300',
@@ -18,6 +29,10 @@ export const TextInputContainer = styled('div', {
     opacity: 0.5,
     cursor: 'not-allowed',
   },
+
+  defaultVariants: {
+    size: 'md'
+  }
 })
 
 export const Prefix = styled('span', {
@@ -44,7 +59,7 @@ export const Input = styled('input', {
     cursor: 'not-allowed',
   },
 
-  '&:placeholder': {
+  '&::placeholder': {
     color: '$gray400',
   },
 })
