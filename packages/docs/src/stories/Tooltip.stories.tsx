23a8507eda45beae9d6339337f2969e64b408c95
Raiane <raiane.oliveira404@gmail.com>
2023-08-29 20:57:41 -0300

    refactor: remove unecessary tooltip variant

--- packages/docs/src/stories/Tooltip.stories.tsx
+++ packages/docs/src/stories/Tooltip.stories.tsx
@@ -5,31 +5,27 @@ export default {
   title: 'Data Display/Tooltip',
   component: Tooltip,
 
-  argTypes: {
-    children: {
-      control: {
-        type: null
-      }
-    }
-  }
-
-} as Meta<TooltipProps>
-
-export const Primary: StoryObj<TooltipProps> = {
-  args: {
-    children: (
-      <Button>21</Button>
-    ),
-    content: '21 de Outubro - Indisponível',
-  },
-}
-
-export const ShowTooltipInstantly: StoryObj<TooltipProps> = {
   args: {
     delayDuration: 0,
     children: (
       <Button>21</Button>
     ),
     content: '21 de Outubro - Indisponível'
+  },
+
+  argTypes: {
+    children: {
+      control: {
+        type: null
+      }
+    },
+    delayDuration: {
+      control: {
+        type: 'number'
+      }
+    },
   }
-}
+
+} as Meta<TooltipProps>
+
+export const Primary: StoryObj<TooltipProps> = {}
